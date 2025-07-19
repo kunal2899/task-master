4c4459670ad6b60dcc7483e7bd9cac386501001d
Kunal Jain <kunal.jain@peoplegrove.com>
2025-07-20 01:13:42 +0530

    feat: added my user routes

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -3,9 +3,19 @@ const router = express.Router();
 
 router.use(express.json());
 
-const { loginUser, registerUser } = require("../controllers/usersController");
+const {
+  loginUser,
+  registerUser,
+  getMyUser,
+  updateMyUser,
+} = require("../controllers/usersController");
 const validateLoginInfo = require("../middlewares/validators/loginUserValidator");
 const validateRegisterUserInfo = require("../middlewares/validators/registerUserValidator");
+const isAuthenticated = require("../middlewares/isAuthenticated");
+const validateUpdateUserInfo = require("../middlewares/validators/updateUserValidator");
+
+router.get("/me", isAuthenticated, getMyUser);
+router.put("/me", isAuthenticated, validateUpdateUserInfo, updateMyUser);
 
 router.post("/login", validateLoginInfo, loginUser);
 router.post("/", validateRegisterUserInfo, registerUser);
