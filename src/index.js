a78b8af464dfdc749d8a41d5d25337295eeb9552
Kunal Jain <kunal.jain@peoplegrove.com>
2025-07-19 23:26:08 +0530

    feat: added entry point file

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
+const connectToDatabase = require('../db/connectionService');
+
 const express = require('express');
+const userRoutes = require('./routes/usersRoutes');
+const teamRoutes = require('./routes/teamsRoutes');
 require('dotenv').config({ quiet: true });
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Connect to database
+connectToDatabase();
+
+// Routes
+app.use('/v1/users', userRoutes);
+app.use('/v1/teams', teamRoutes);
 
 app.listen(PORT, () => {
   console.info('Server is listening on port', PORT);
